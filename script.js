971dce1544eeea587ba516312daba9fdb24e66ff
Dzyree <whytedzyre@gmail.com>
2025-01-23 08:50:42 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+const PI = 3.14;
+let r = 6.378e6;
+let C = 2*pi*r;
+let A = (4*pi*(r**2));
+let V = (4/3*pi*(r**3));
+
+document.write('The radius of the sphere is ' + r + '.');
+document.write('<br><br>');
+document.write('The circumference is ' + C + '.');
+document.write('<br><br>');
+document.write('The surface area is ' + A + '.');
+document.write('<br><br>');
+document.write('The volume is ' + V + '.');
+document.write('<br><br>');
+
+
